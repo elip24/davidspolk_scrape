53c73bc0486d08a4e3f13b0a614b21cb922e159d
Elio <82463551+elip24@users.noreply.github.com>
2025-08-04 07:26:11 -0300

    Add files via upload

--- src/ingestion/ingestion.py
+++ src/ingestion/ingestion.py
@@ -1,11 +1,14 @@
 from playwright.async_api import async_playwright
 
 from get_profile_ingestion import get_url_profiles
+from news_only import get_news_articles
 from profile_only import get_profile_information
 from asyncio import Semaphore
 import asyncio
 from concurrency_utils import process_with_semaphore
 import json
+
+from src.ingestion.get_news_ingestion import get_urls_news
 
 MAX_CONCURRENT_REQUESTS = 5
 location_list=['New York','Northern California','Washington DC']
@@ -29,7 +32,24 @@ async def main():
         with open("davidspolk_profile.json", "w", encoding="utf-8") as f:
             json.dump(all_profiles, f, indent=2, ensure_ascii=False)
 
-asyncio.run(main())
+
+async def main_news():
+    urls_to_scrape=get_urls_news(start_url=start_url)
+    async with async_playwright() as playwright:
+        semaphore = Semaphore(MAX_CONCURRENT_REQUESTS)
+        all_news = []
+        for url in urls_to_scrape:
+            try:
+                news_data = await process_with_semaphore(playwright=playwright,url=url,semaphore=semaphore,
+                                                            extractor_function=get_news_articles)
+                all_news.append(news_data)
+            except Exception as e:
+                print(f"Failed to extract {url}: {e}")
+                return None
+        with open("davidspolk_news.json", "w", encoding="utf-8") as f:
+            json.dump(all_news, f, indent=2, ensure_ascii=False)
+
+asyncio.run(main_news())
 
 
 
--- src/test/get_article_only.py
+++ src/test/get_article_only.py
@@ -4,34 +4,34 @@ from src.ingestion.page_configurations import custom_headers
 
 start_url='https://www.davispolk.com/firm-news/victory-ucla-students-and-faculty-campus-antisemitism-case/'
 
-async def get_each_reference(page,search_reference):
+def get_each_reference(page,search_reference):
     reference_section = page.locator(f'[data-accordion-title="{search_reference}"]')
     selection_references = reference_section.locator("a.link--internal")
     each_reference = []
-    count=await selection_references.count()
+    count=selection_references.count()
     for i in range(count):
-        href =await  selection_references.nth(i).get_attribute("href")
+        href = selection_references.nth(i).get_attribute("href")
         each_reference.append(href)
     return each_reference
 
-async def extract_article_text(page):
-    paragraphs =await page.locator("#article-body-field p").all_inner_texts()
+def extract_article_text(page):
+    paragraphs =page.locator("#article-body-field p").all_inner_texts()
     return "\n\n".join(p.strip() for p in paragraphs)
 
-async def get_news_articles(playwright, start_url):
-    browser =await playwright.chromium.launch()
-    context =await browser.new_context(extra_http_headers=custom_headers)
-    page = await context.new_page()
-    await page.goto(start_url)
-    data =await page.locator("script[type='application/ld+json']").text_content()
+def get_news_articles(playwright, start_url):
+    browser =playwright.chromium.launch()
+    context =browser.new_context(extra_http_headers=custom_headers)
+    page = context.new_page()
+    page.goto(start_url)
+    data =page.locator("script[type='application/ld+json']").text_content()
     json_raw=json.loads(data)
     graph=json_raw.get("@graph", [{}])[0]
     headline=graph.get("headline")
     datePublished=graph.get("datePublished")
-    capabilities_link=await get_each_reference(page,search_reference='Related capabilities')
-    lawyers_link=await get_each_reference(page,search_reference='Related lawyers')
-    id=await page.get_by_label('Download PDF').get_attribute("href")
-    text =await extract_article_text(page)
+    capabilities_link=get_each_reference(page,search_reference='Related capabilities')
+    lawyers_link=get_each_reference(page,search_reference='Related lawyers')
+    id=page.get_by_label('Download PDF').get_attribute("href")
+    text =extract_article_text(page)
 
     all_profile = {
         "headline": headline,
@@ -43,7 +43,9 @@ async def get_news_articles(playwright, start_url):
         "text": text,
         "json_raw": json_raw
     }
-    await page.close()
-    await context.close()
-    await browser.close()
     return all_profile
+
+
+
+with sync_playwright() as p:
+    get_news_articles(p,start_url=start_url)
